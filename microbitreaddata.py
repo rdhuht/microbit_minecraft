f0c52410b74802c84fdaa82b22614b3344c0cadb
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2020-07-30 22:46:12 +0800

    Update microbitreaddata.py

--- microbitreaddata.py
+++ microbitreaddata.py
@@ -1,13 +1,12 @@
 from microbit import *
 
-REFRESH = 50
+REFRESH = 500
 
 
 def get_data():
     x, y, z = accelerometer.get_x(), accelerometer.get_y(), accelerometer.get_z()
     a, b = button_a.was_pressed(), button_b.was_pressed()
     print(x, y, z, a, b)
-
 
 def run():
     while True:
