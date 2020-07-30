edab48edbaf8ec484b5c60a0a2128fc98c66815d
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2020-07-30 22:46:00 +0800

    trivial

---
+++ readDataFromMicrobit.py
@@ -0,0 +1,26 @@
+import serial
+
+PORT = "COM10"
+BAUD = 115200
+
+s = serial.Serial(PORT)
+s.baudrate = BAUD
+s.parity = serial.PARITY_NONE
+s.databits = serial.EIGHTBITS
+s.stopbits = serial.STOPBITS_ONE
+
+try:
+    while True:
+        # print(s.readline())
+        data = str(s.readline()).rstrip()[2:-9].split(' ')
+        # print(type(data))
+        # print(data)
+        # print(len(data))
+        if len(data) > 1:
+            x, y, z = data[0], data[1], data[2]
+            a, b = data[3], data[4]
+            print(x, y, z, a, b, type(x), type(a))
+
+
+finally:
+    s.close()
